526b75fe75be81dc65d7e6dba5a748f14006a51e
gabrielgessen <66341065+gabrielgessen@users.noreply.github.com>
2021-12-01 19:59:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-from networkx.algorithms import graph_hashing
+#import networkx as nx
+##import matplotlib.pyplot as plt
+#from networkx.algorithms import graph_hashing
 from node import *
 from graph import *
 
@@ -42,10 +42,15 @@ def create():
             id = int(input("Ingrese el id de la actividad: "))
             descripcion = str(input("Ingrese la descripcion de la actividad: "))
             duracion = float(input("Ingrese la duracion de la actividad: "))      
-            pre = input("Ingrese los ids de sus predecesores separados por comas: ") 
-
-            graph.add_node(id, descripcion, duracion, pre.split(","))
+            pre = input("Ingrese los ids de sus predecesores separados por comas: ")
+            pre= pre.split(",") 
+            pres = []
             for p in pre:
+                pres.append(int(p))
+            graph.add_node(id, descripcion, duracion, pres)
+            
+            for p in pres:
+                print(p)
                 graph.nodes_dict[p].add_sucesor(id)
                 
             nodesId.append(id)
@@ -54,27 +59,13 @@ def create():
             print ("2. No")
             loop = input("Ingrese 1 o 2 segun corresponda: ")
 
-        ## esto de abajo deberia ir en otro lado
-        sin_suc = []
+        
 
-        for node in graph.nodes_dict.items():
-            if len(node[1].suces) == 0:
-                sin_suc.append(node[1].id)
-
-        if len(sin_suc) == 1:
-            return
-        else:
-            print("No se puede tener un grafo abierto. Escoga el id de su nodo final: ")
-            for n in sin_suc:
-                print(n)
-            nodo_final = input()
-            sin_suc.remove(nodo_final)
-            for n in sin_suc:
-                graph.nodes_dict[n].add_sucesor(nodo_final)
+        
             
 
 
-    return
+    return graph
 
 
 
@@ -86,7 +77,24 @@ def create():
 
 
 def main():
-    create()
+    graph = create()
+    ## esto de abajo deberia ir en otro lado
+    sin_suc = []
+    for node in graph.nodes_dict.items():
+            print(str(node[1].id)+" cantidad de nodos siguientes->"+str(len(node[1].suces)))
+            if len(node[1].suces) == 0:
+                sin_suc.append(node[1].id)
+
+    if len(sin_suc) == 1:
+        return
+    else:
+        print("No se puede tener un grafo abierto. Escoga el id de su nodo final: ")
+        for n in sin_suc:
+            print(n)
+        nodo_final = input()
+        sin_suc.remove(nodo_final)
+        for n in sin_suc:
+            graph.nodes_dict[n].add_sucesor(nodo_final)
     print('main proyecto')
 
 
